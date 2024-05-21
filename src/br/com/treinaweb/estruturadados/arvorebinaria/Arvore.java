17dd12625a01be12b81d3b89967c3936709d575a
Jadson Lima <jadsonhenrique2004@gmail.com>
2024-05-21 09:26:45 -0300

    Implementando algoritmos recursivos no processo de inserção de nós em uma árvore binária

--- src/br/com/treinaweb/estruturadados/arvorebinaria/Arvore.java
+++ src/br/com/treinaweb/estruturadados/arvorebinaria/Arvore.java
@@ -18,8 +18,23 @@ public class Arvore<T>  {
         if (this.raiz == null) {
             this.raiz = no;
         } else if (raiz.peso() < no.peso()) {
-            this.raiz.setNoDireito(no);
+            this.inserir(this.raiz, no);
         }
     }
 
+    private void inserir(NoArvore<T> ref, NoArvore<T> novoNo) {
+        if (ref.peso() < novoNo.peso()) {
+            if (ref.getNoDireito() == null) {
+                ref.setNoDireito(novoNo);
+            } else {
+                inserir(ref.getNoDireito(), novoNo);
+            }
+        } else {
+            if (ref.getNoEsquerdo() == null) {
+                ref.setNoEsquerdo(novoNo);
+            } else {
+                inserir(ref.getNoEsquerdo(), novoNo);
+            }
+        }
+    }
 }
